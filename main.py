38bc6ced5f5eeef46c7a56889da38c2bea623e76
MercyTheHunter <vargajeff21@gmail.com>
2024-04-20 14:25:40 +0200

    model update - p3k7

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import models as m
 import functions as func
 import os
 
-dataset = "MNIST"
-#dataset = "FashionMNIST"
-#dataset = "CATDOG"
-
-filename = dataset + "_CNN_small"
-#filename = dataset + "_CNN_medium"
-#filename = dataset + "_FNN_small"
-#filename = dataset + "_FNN_medium"
-
-savedmodelname = dataset + "_CNN_small.pkl"
-#savedmodelname = dataset + "_CNN_medium.pkl"
-#savedmodelname = dataset + "_FNN_small.pkl"
-#savedmodelname = dataset + "_FNN_medium.pkl"
-
-current_path = os.path.dirname(os.path.realpath(__file__))
 patience = 3 #3 or 5 or 7 (for save location)
 kernel = 7 #3 or 5 or 7 (for save location)
+model = 4 #1:CNN_small, 2:CNN_medium, 3:FNN_small, 4:FNN_medium
+classes = 10 #MNIST: 10, FashionMNIST: 10, CATDOG: 2
+
+#dataset = "MNIST"
+dataset = "FashionMNIST"
+#dataset = "CATDOG"
+
+if model == 1:
+    filename = dataset + "_CNN_small"
+elif model == 2:
+    filename = dataset + "_CNN_medium"
+elif model == 3:
+    filename = dataset + "_FNN_small"
+elif model == 4:
+    filename = dataset + "_FNN_medium"
+
+if model == 1:
+    savedmodelname = filename + ".pkl"
+elif model == 2:
+    savedmodelname = filename + ".pkl"
+elif model == 3:
+    savedmodelname = filename + ".pkl"
+elif model == 4:
+    savedmodelname = filename + ".pkl"
+
+current_path = os.path.dirname(os.path.realpath(__file__))
 
 if dataset == "MNIST":
     train_loader, valid_loader, test_loader = func.MNIST_make_loaders(batch_size=256)
@@ -39,17 +50,21 @@ if os.path.isfile(os.path.join(current_path, "SavedModels/", savedmodelname)):
     print("Loading the saved model...")
     model = func.load_model(savedmodelname)
 else:
-    model = m.Conv_NN_small()
-    #model = m.Conv_NN_medium()
-    #model = m.FNO_NN_small()
-    #model = m.FNO_NN_medium()
+    if model == 1:
+        model = m.Conv_NN_small(kernelsize=kernel, classes=classes)
+    elif model == 2:
+        model = m.Conv_NN_medium(kernelsize=kernel, classes=classes)
+    elif model == 3:
+        model = m.FNO_NN_small(kernelsize=kernel, classes=classes)
+    elif model == 4:
+        model = m.FNO_NN_medium(kernelsize=kernel, classes=classes)
 
     print(f"Training the {savedmodelname} model on the {dataset} dataset")
 
     model, train_losses, valid_losses = func.train_model(model=model,
                                                          train_loader=train_loader,
                                                          valid_loader=valid_loader,
-                                                         patience=patience, #3 or 5 or 7
+                                                         patience=patience,
                                                          n_epochs=200)
     func.save_model(model, savedmodelname, patience, kernel)
     func.loss_plot(train_loss=train_losses, 
--- models.py
+++ models.py
@@ -5,13 +5,17 @@ import torch.nn.functional as F
 ##############################################################
 #   2D Convolutional Neural Networks
 ##############################################################
-
 class Conv_NN_small(nn.Module):
-    def __init__(self):
+    def __init__(self, kernelsize, classes):
         super(Conv_NN_small, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1)
-        self.fc1 = nn.Linear(12*12*32,256)  #kernel:3 -> 13*13*32; kernel:5 -> 12*12*32; kernel:7 -> 11*11*32
-        self.fc2 = nn.Linear(256,10)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=kernelsize, stride=1)
+        if (kernelsize == 3):
+            self.fc1 = nn.Linear(13*13*32,256) #kernel:3 -> 13*13*32;
+        elif (kernelsize == 5):
+            self.fc1 = nn.Linear(12*12*32,256) #kernel:5 -> 12*12*32;
+        elif (kernelsize == 7):
+            self.fc1 = nn.Linear(11*11*32,256) #kernel:7 -> 11*11*32
+        self.fc2 = nn.Linear(256,classes)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
@@ -31,12 +35,17 @@ class Conv_NN_small(nn.Module):
 
         return x
 class Conv_NN_medium(nn.Module):
-    def __init__(self):
+    def __init__(self, kernelsize, classes):
         super(Conv_NN_medium, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1) #28 x 28 x 1 -> 26 x 26 x 16 -> 13 x 13 x 16
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1) #13 x 13 x 16 -> 11 x 11 x 32 -> 5 x 5 x 32
-        self.fc1 = nn.Linear(4*4*32,256)    #kernel:3 -> 5*5*32; kernel:5 -> 4*4*32; kernel:7 -> 2*2*32
-        self.fc2 = nn.Linear(256,10)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=kernelsize, stride=1) #28 x 28 x 1 -> 26 x 26 x 16 -> 13 x 13 x 16
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=kernelsize, stride=1) #13 x 13 x 16 -> 11 x 11 x 32 -> 5 x 5 x 32
+        if (kernelsize == 3):
+            self.fc1 = nn.Linear(5*5*32,256) #kernel:3 -> 5*5*32;
+        elif (kernelsize == 5):
+            self.fc1 = nn.Linear(4*4*32,256) #kernel:5 -> 4*4*32;
+        elif (kernelsize == 7):
+            self.fc1 = nn.Linear(2*2*32,256) #kernel:7 -> 2*2*32
+        self.fc2 = nn.Linear(256,classes)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
@@ -118,11 +127,11 @@ class FourierLayer(nn.Module):
 #   Neural Networks Using the 2D Fourier Layer - Fourier Neural Operator
 ###########################################################################
 class FNO_NN_small(nn.Module):
-    def __init__(self):
+    def __init__(self, kernelsize, classes):
         super(FNO_NN_small, self).__init__()
-        self.fno1 = FourierLayer(in_channels=1, out_channels=32, kernel_size=5, padding=5//2, stride=1, modes1=4, modes2=4) 
-        self.fc1 = nn.Linear(14*14*32,256) #kernel:3 -> 14*14*32; kernel:5 -> 14*14*32; kernel:7 -> 14*14*32
-        self.fc2 = nn.Linear(256,10)
+        self.fno1 = FourierLayer(in_channels=1, out_channels=32, kernel_size=kernelsize, padding=kernelsize//2, stride=1, modes1=4, modes2=4) 
+        self.fc1 = nn.Linear(14*14*32,256)
+        self.fc2 = nn.Linear(256,classes)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
@@ -142,12 +151,12 @@ class FNO_NN_small(nn.Module):
 
         return x
 class FNO_NN_medium(nn.Module):
-    def __init__(self):
+    def __init__(self, kernelsize, classes):
         super(FNO_NN_medium, self).__init__()
-        self.fno1 = FourierLayer(in_channels=1, out_channels=16, kernel_size=5, padding=5//2, stride=1, modes1=4, modes2=4) 
-        self.fno2 = FourierLayer(in_channels=16, out_channels=32, kernel_size=5, padding=5//2, stride=1, modes1=4, modes2=4) 
-        self.fc1 = nn.Linear(7*7*32,256)    #kernel:3 -> 7*7*32; kernel:5 -> 7*7*32; kernel:7 -> 7*7*32
-        self.fc2 = nn.Linear(256,10)
+        self.fno1 = FourierLayer(in_channels=1, out_channels=16, kernel_size=kernelsize, padding=kernelsize//2, stride=1, modes1=4, modes2=4) 
+        self.fno2 = FourierLayer(in_channels=16, out_channels=32, kernel_size=kernelsize, padding=kernelsize//2, stride=1, modes1=4, modes2=4) 
+        self.fc1 = nn.Linear(7*7*32,256)
+        self.fc2 = nn.Linear(256,classes)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
