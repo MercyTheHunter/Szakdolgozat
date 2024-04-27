88edf53360ac2f4e8318dc5c783a975b8d3881c3
MercyTheHunter <vargajeff21@gmail.com>
2024-04-27 12:24:24 +0200

    main small update

--- main.py
+++ main.py
@@ -6,15 +6,39 @@ import os
 patience = 3 #3 or 5 or 7 training parameter (also used for save location)
 kernel = 3 #3 or 5 or 7 model parameter (also used for save location)
 model = 1 #1:CNN_small, 2:CNN_medium, 3:CNN_big, 4:FNN_small, 5:FNN_medium, 6:FNN_big
+data = 1 #1: MNIST, 2: FashionMNIST, 3: CATDOG
 
 mode = 1 #Training: 1, Testing: 2
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 
-dataset = "MNIST"
-#dataset = "FashionMNIST"
-#dataset = "CATDOG"
+#Loading the dataset + setting base parameters + Plot of example images
+if data == 1:
+    dataset = "MNIST"
+    classes = 10
+    im_size = 28
+    in_channels = 1
+    train_loader, valid_loader, test_loader = func.MNIST_make_loaders(batch_size=128)
+    #func.example_plot(train_loader=train_loader,
+    #                  dataset=dataset)
+elif data == 2:
+    dataset = "FashionMNIST"
+    classes = 10
+    im_size = 28
+    in_channels = 1
+    train_loader, valid_loader, test_loader = func.FashionMNIST_make_loaders(batch_size=128)
+    #func.example_plot(train_loader=train_loader,
+    #                  dataset=dataset)
+elif data == 3:
+    dataset = "CATDOG"
+    classes = 2
+    im_size = 224
+    in_channels = 3
+    train_loader, valid_loader, test_loader, class_names = func.CATDOG_make_loaders(batch_size=128)
+    #func.example_plot(train_loader=train_loader,
+    #                  dataset=dataset)
 
+#Set the current model name for saving
 if model == 1:
     filename = dataset + "_CNN_small"
 elif model == 2:
@@ -29,33 +53,12 @@ elif model == 6:
     filename = dataset + "_FNN_big"
 
 savedmodelname = filename + ".pkl"
-
-#Loading the dataset + Plot of example images
-if dataset == "MNIST":
-    classes = 10
-    im_size = 28
-    in_channels = 1
-    train_loader, valid_loader, test_loader = func.MNIST_make_loaders(batch_size=128)
-    #func.example_plot(train_loader=train_loader,
-    #                  dataset=dataset)
-elif dataset == "FashionMNIST":
-    classes = 10
-    im_size = 28
-    in_channels = 1
-    train_loader, valid_loader, test_loader = func.FashionMNIST_make_loaders(batch_size=128)
-    #func.example_plot(train_loader=train_loader,
-    #                  dataset=dataset)
-elif dataset == "CATDOG":
-    classes = 2
-    im_size = 224
-    in_channels = 3
-    train_loader, valid_loader, test_loader, class_names = func.CATDOG_make_loaders(batch_size=128)
-    #func.example_plot(train_loader=train_loader,
-    #                  dataset=dataset)
+    
 
 #Loading a trained model or training a new one
-if os.path.isfile(os.path.join(current_path, "SavedModels/", savedmodelname)):
-    print("There is a saved model.")
+if mode == 2:
+    #WIP!!
+    print("Testing saved models...")
     print("Loading the saved model...")
     model = func.load_model(model_name=savedmodelname)
 else:
